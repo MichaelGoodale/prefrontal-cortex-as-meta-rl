Fixed training (was backpropagating through actions)

cumulative_regret.py
@@ -21,25 +21,27 @@ def cumulative_regret(probabilities, rewards):
         regrets.append(expected_sum - reward_sum)
     return regrets
 
-def run_episode(env, model, probs=None):
+def run_episode(env, model, probs=None, return_values=False):
     state = env.reset(probs)
     done = False
     reward = 0.0
     rewards = []
     actions = []
+    values = []
     action = torch.tensor(0)
     hidden = None
     cell = None
-    t = 0 
     with torch.no_grad():
         while not done:
             value, action_space, (hidden, cell) = model(state, reward, action, hidden=hidden, cell=cell)
             action_distribution = distributions.Categorical(action_space)
             action = action_distribution.sample()
             state, reward, done, _ = env.step(action.item())
-            t += 1
             actions.append(action.item())
             rewards.append(reward)
+            values.append(value.item())
+        if return_values:
+            return actions, rewards, values
         return actions, rewards
 
 def get_regrets(model_path, probabilities=[0.75, 0.25], n_samples=500):
@@ -104,8 +106,6 @@ def pca_plot(model_path, probabilities=[0.99, 0.01]):
     activation_mat = torch.cat([x[0] for x in activations['lstm']]).squeeze().numpy()
     activation_x_y = pca.transform(activation_mat)
 
-
-
     fig, ax = plt.subplots()
     ax.set_xlabel("PCA 1")
     ax.set_ylabel("PCA 2")
@@ -114,8 +114,3 @@ def pca_plot(model_path, probabilities=[0.99, 0.01]):
     ax.scatter(activation_x_y[~l_idx, 0], activation_x_y[~l_idx, 1], c=np.arange(100)[~l_idx], marker='x', cmap='copper')
     ax.set_title(f"P_l={probabilities[0]}, P_r={probabilities[1]}")
     plt.show()
-
-#independent_bandit_regret, _, _ = get_regrets("model_bandit_uncorr.pt")
-#correlated_bandit_regret, _, _ = get_regrets("model_bandit_corr.pt")
-#cumulative_regret_plot(independent_bandit_regret, correlated_bandit_regret)
-pca_plot("model_bandit_corr.pt", [0.2, 0.8])

train_bandits.py
@@ -11,7 +11,7 @@ import gym
 model = PrefrontalLSTM(0, 2)
 model.train()
 optimizer = torch.optim.RMSprop(model.parameters(), lr=0.0007)
-env = TaskOne(mode='bandit_uncorr')
+env = TaskOne(mode='monkey')
 loss = []
 rewards = []
 t_range = tqdm(range(30000))
@@ -21,7 +21,7 @@ for i in t_range:
     loss.append(l)
     rewards.append(sum(r) / len(r))
 
-torch.save(model.state_dict(), 'model_bandit_uncorr.pt')
+torch.save(model.state_dict(), 'monkey_action_item.pt')
 fig, axs = plt.subplots(2)
 
 def smooth(X):